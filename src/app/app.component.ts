51e44d28f5b71997cc35200f9078d069bce3b377
Mayra Rodriguez <mayrarodriguez@Mayras-MacBook-Pro.local>
2023-11-25 07:30:35 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,78 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component, inject} from '@angular/core';
+import {CommonModule, NgClass, NgFor, NgIf} from '@angular/common';
+import {addDoc, collection, Firestore, onSnapshot} from "@angular/fire/firestore";
 
+interface DisplayMessage {
+  text: string;
+  type: 'PROMPT' | 'RESPONSE';
+}
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgFor, NgClass, NgIf],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+
 })
 export class AppComponent {
   title = 'chatbot-palm-demo';
+
+  private readonly firestore: Firestore = inject(Firestore);
+  private readonly discussionCollection = collection(this.firestore, 'discussions');
+  prompt = '';
+  status = '';
+  errorMsg = '';
+  responses: DisplayMessage[] = [
+    {
+      text: "Hello Mayra, How can I help you today?",
+      type: 'RESPONSE'
+    }
+  ];
+
+  async submitPrompt(event: Event, promptText: HTMLInputElement) {
+    event.preventDefault();
+
+    if (!promptText.value) return;
+    this.prompt = promptText.value;
+    promptText.value = '';
+    this.responses.push({
+      text: this.prompt,
+      type: 'PROMPT',
+    });
+
+    this.status = 'Loading...';
+    const discussionDoc = await addDoc(this.discussionCollection, { prompt: this.prompt });
+
+    const destroyFn = onSnapshot(discussionDoc, {
+      next: snap => {
+        const conversation = snap.data();
+        if (conversation && conversation['status']) {
+          const state = conversation['status']['state'];
+
+          switch (state) {
+            case 'COMPLETED':
+              this.status = '';
+              this.responses.push({
+                text: conversation['response'],
+                type: 'RESPONSE',
+              });
+              destroyFn();
+              break;
+            case 'PROCESSING':
+              this.status = 'preparing your answer...';
+              break;
+            case 'ERRORED':
+              this.status = 'Oh no! Something went wrong. Please try again.';
+              destroyFn();
+              break;
+          }
+        }
+      },
+      error: err => {
+        console.log(err);
+        this.errorMsg = err.message;
+        destroyFn();
+      }
+    })
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,13 @@
-import { ApplicationConfig } from '@angular/core';
+import {ApplicationConfig, importProvidersFrom} from '@angular/core';
+import {initializeApp, provideFirebaseApp} from "@angular/fire/app";
+import {getFirestore, provideFirestore} from "@angular/fire/firestore";
+import {environment} from "../environments/environment";
 
 export const appConfig: ApplicationConfig = {
-  providers: []
+  providers: [
+    importProvidersFrom(
+      provideFirebaseApp(() => initializeApp(environment.firebase)),
+      provideFirestore(() => getFirestore()),
+    )
+  ]
 };
